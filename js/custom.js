f3244eaba5d217ba449df7be48a7a4a9e510944a
mayur-412 <mayurzezariya990@gmail.com>
2026-09-16 15:16:16 -0400

    add
    add

--- js/custom.js
+++ js/custom.js
@@ -25,10 +25,12 @@ $('.tab ul li').click(function(){
   return false;
 });
 
-$('.owl-carousel').owlCarousel({
+$('.six-section .owl-carousel').owlCarousel({
     loop:true,
     margin:10,
     nav:true,
+    autoplay:true,
+    autoplayTimeout:3000,
     responsive:{
         0:{
             items:1
@@ -38,3 +40,56 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+$('.seven-section .owl-carousel').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:3
+        }
+    }
+})
+
+// Trigger CSS animations on scroll.
+// Detailed explanation can be found at http://www.bram.us/2013/11/20/scroll-animations/
+
+// Looking for a version that also reverses the animation when
+// elements scroll below the fold again?
+// --> Check https://codepen.io/bramus/pen/vKpjNP
+
+jQuery(function($) {
+  
+  // Function which adds the 'animated' class to any '.animatable' in view
+  var doAnimations = function() {
+    
+    // Calc current offset and get all animatables
+    var offset = $(window).scrollTop() + $(window).height(),
+        $animatables = $('.animatable');
+    
+    // Unbind scroll handler if we have no animatables
+    if ($animatables.length == 0) {
+      $(window).off('scroll', doAnimations);
+    }
+    
+    // Check all animatables and animate them if necessary
+        $animatables.each(function(i) {
+       var $animatable = $(this);
+            if (($animatable.offset().top + $animatable.height() - 20) < offset) {
+        $animatable.removeClass('animatable').addClass('animated');
+            }
+    });
+
+    };
+  
+  // Hook doAnimations on scroll, and trigger a scroll
+    $(window).on('scroll', doAnimations);
+  $(window).trigger('scroll');
+
+});
